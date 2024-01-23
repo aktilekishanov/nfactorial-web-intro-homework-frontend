b9e6a2daddeee32a93f456e4f2eae67287bc0e5e
Aktilek Ishanov <aktilek.ishanov@gmail.com>
2024-01-23 23:22:16 +0600

    homework 5

---
+++ hw5/index.js
@@ -0,0 +1,18 @@
+// TASK 1
+let table = document.getElementById('age-table')
+
+// TASK 2
+table.getElementsByTagName('label')
+
+// TASK 3
+table.rows[0].cells[0]
+
+// TASK 4
+document.querySelector('form[name="search"]')
+
+// TASK 5
+form.getElementsByTagName('input')[0]
+
+// TASK 6
+const inputs = form.querySelectorAll("input") 
+inputs[inputs.length-1]
